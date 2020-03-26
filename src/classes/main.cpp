3c06e2f21cc2007d587fbdc390bc3ae6902edfae
eleve <carrascosa.arnaud@lyceeppr.fr>
2020-03-26 13:04:28 +0100

    Finalisation du projet

--- src/classes/main.cpp
+++ src/classes/main.cpp
@@ -4,17 +4,27 @@ using namespace std;
 
 int main()
 {
-	Etudiant e;
+	/*Déclaration des variables*/
 	string nom;
 	int note1,note2;
 
+	/*Demandes d'entrées utilisateur*/
 	cout << "Veuillez renter votre nom\n";
 	cin >> nom;
 	cout << "Veuillez renter votre note/20\n";
 	cin >> note2;
-	cout << "Veuillez renter votre note/10\n\n";
+	cout << "Veuillez renter votre note/10\n";
 	cin >> note1;
-	cout << e.getNom() << endl << e.getNoteSur20() << endl << e.getNoteSur10() << endl;
-	cout << e.calculerMoyenne(note1,note2) << endl;
+
+	Etudiant e(nom,note2,note1);//Création de l'objet
+
+	/*Affichage*/
+	cout << "\n\nNom de l'etudiant : \n" << e.getNom() << endl;
+	cout << "Note sur 20 : \n" << e.getNoteSur20() << endl;
+	cout << "Note sur 10 : \n" << e.getNoteSur10() << endl << endl;
+	cout << e.getNom() << " : " << e.getNoteSur20() << "/20\t" << e.getNoteSur10() << "/10\n";
+	cout << "Moyenne : " << e.calculerMoyenne(note1,note2) << endl;
+
+	
 	return 0;
 }
